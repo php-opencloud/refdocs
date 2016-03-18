a5efaaeb624113807dc18f364f21e5a230aff76a
Jamie Hannaford <jamie.hannaford@rackspace.com>
2016-03-18 14:52:32 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,17 +5,16 @@ cd /var/www/tmp
 if [ -d "openstack" ]; then
   cd openstack
   git pull
-  composer update --prefer-source --no-interaction
+  composer update --no-interaction
 else
   git clone https://github.com/php-opencloud/openstack
   cd openstack
-  composer install --prefer-source --no-interaction
+  composer install --no-interaction
 fi
 
 # Build docs
-rm -rf ./doc/api-docs/cache
-./vendor/bin/sami.php update ./doc/api-docs/config.php
+./vendor/bin/sami.php update --force ./doc/api-docs/config.php
 
 # Shift tmp dir to prod
-rm -rf /var/www/html/build
+rm -rf /var/www/html/*
 mv ./doc/api-docs/build /var/www/html
